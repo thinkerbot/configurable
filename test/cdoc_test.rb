042a7eb259ab6bd74e9deb1658495f6ad38b1871
Simon Chiang <simon.a.chiang@gmail.com>
2010-03-28 00:25:39 -0600

    cleanup of require paths

--- test/cdoc_test.rb
+++ test/cdoc_test.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__), 'tap_test_helper')
+require File.expand_path('../tap_test_helper.rb', __FILE__) 
 require 'fileutils'
 
 class CDocTest < Test::Unit::TestCase
--- test/configurable/nest_config_test.rb
+++ test/configurable/nest_config_test.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__), '../tap_test_helper')
+require File.expand_path('../../tap_test_helper.rb', __FILE__) 
 require 'configurable/nest_config'
 
 class NestConfigTest < Test::Unit::TestCase
