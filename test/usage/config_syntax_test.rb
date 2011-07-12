a7d01b8b8c559a747e139a4780dfc0e95880d906
Simon Chiang <simon.a.chiang@gmail.com>
2011-07-12 15:49:25 -0600

    moved usage tests

--- test/usage/config_syntax_test.rb
+++ test/usage/config_syntax_test.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../../test_helper', __FILE__)
 require 'configurable'
 
-class BasicSyntaxTest < Test::Unit::TestCase
+class ConfigSyntaxTest < Test::Unit::TestCase
   class ConfigClass
     include Configurable
     config :str, 'one'
